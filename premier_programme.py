fc755466709ce5c38ccc46cd34ce68ff918292a6
Fezeer <91223800+Fezeer@users.noreply.github.com>
2021-09-27 12:26:49 +0200

    Update premier_programme.py

--- premier_programme.py
+++ premier_programme.py
@@ -1,2 +1,6 @@
 prenom = input("Quel est votre nom ? ")
 print("Bonjour {}, comment allez vous aujourd'hui ?".format(prenom))
+
+birth_year = int(input("En quelle année es-tu né: "))
+age = 2021 - birth_year
+print(f"tu as {age} ans")
